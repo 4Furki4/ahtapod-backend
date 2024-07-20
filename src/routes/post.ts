ee93286e9594ccf42d0bf5fb3f61ba7a009176f5
Furkan Cengiz <muhammedcengiz1@gmail.com>
2024-07-20 20:20:34 +0300

    add: pagination and user details to /posts endpoint.

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -7,15 +7,38 @@ import { ClerkExpressRequireAuth } from '@clerk/clerk-sdk-node'
 export const postRouter = express.Router()
 
 postRouter.get('/', async (req, res) => {
-    const { page, limit } = req.query
+    const { page = 1, limit = 10 } = req.query
     const posts = await prisma.post.findMany({
         take: limit ? Number(limit) : 10,
-        skip: page ? (Number(page) - 1) * (limit ? Number(limit) : 10) : 0,
+        skip: page ? (Number(page) - 1) * Number(limit) : 0,
         orderBy: {
             createdAt: 'desc',
         },
     })
-    res.json(posts)
+    const postWithUsers = await Promise.all(posts.map(async post => {
+        const user = await prisma.user.findUnique({
+            where: {
+                clerkUserId: post.userId,
+            },
+            select: {
+                id: true,
+                email: true,
+                firstName: true,
+                lastName: true,
+                imageUrl: true,
+            }
+        })
+        return {
+            ...post,
+            user,
+        }
+    }))
+    const totalPosts = await prisma.post.count()
+    res.json({
+        posts: postWithUsers,
+        totalPosts,
+        totalPages: Math.ceil(totalPosts / Number(limit)),
+    })
 })
 
 postRouter.post('/', ClerkExpressRequireAuth(), validateData(postAddSchema), async (req, res) => {
